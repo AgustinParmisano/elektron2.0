c74357b4811391ad9ca1a5bc5c6b3b34baf45f8f
agustinparmisano <agustinparmisano@gmail.com>
2017-10-10 11:03:25 -0300

    Terminando de desarrollar y ahora testeando la DateTimeTask

--- elektron_django/elektron_django/tasks_handler.py
+++ elektron_django/elektron_django/tasks_handler.py
@@ -5,8 +5,31 @@ from Queue import Queue
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
-datetime_tasks_q = Queue()
 
+def get_tasks_from_server(server_ip, server_port, task_type):
+    tasks_q = Queue()
+    print str(task_type)
+    tasks_get = requests.get("http://" + server_ip + ":" + server_port + "/tasks/" + str(task_type))
+
+    if tasks_get.status_code == 200:
+        tasks = json.loads(tasks_get.text)[str(task_type)]
+        ready_tasks = []
+
+        for dt in tasks:
+            print dt["label"] + " " + dt["taskstate"]["name"]
+            print ""
+            if dt["taskstate"]["name"] == "ready":
+                ready_tasks.append(dt)
+
+        sorted_ready_tasks = sorted(ready_tasks, key=lambda k: k['created'])
+
+        for srt in sorted_ready_tasks:
+            tasks_q.put(srt)
+        return tasks_q
+    else:
+        print "Error in Django Server: " + str(tasks_get.status_code)
+        return False
+"""
 def get_datatasks_from_server(server_ip, server_port):
     data_tasks_q = Queue()
     datatasks_get = requests.get("http://" + server_ip + ":" + server_port + "/tasks/datatasks")
@@ -16,8 +39,8 @@ def get_datatasks_from_server(server_ip, server_port):
         for dt in datatasks:
             print dt["label"] + " " + dt["taskstate"]["name"]
             print ""
-            if dt["taskstate"]["name"] == "ready":
                 ready_datatasks.append(dt)
+                if dt["taskstate"]["name"] == "ready":
 
         sorted_ready_datatasks = sorted(ready_datatasks, key=lambda k: k['created'])
 
@@ -27,7 +50,7 @@ def get_datatasks_from_server(server_ip, server_port):
     else:
         print "Error in Django Server: " + str(datatasks.status_code)
         return False
-
+"""
 def data_date_is_greater(d,l):
     data_date_greater_list = []
     for i in range(0,len(l)):
@@ -41,16 +64,42 @@ def data_date_is_greater(d,l):
 def data_is_greater(d,l):
     for i in range(0,len(l)):
         if int(l[i]["data_value"]) > int(d):
-            print l[i]
             return i
     return -1
 
-def data_correct(tdate,tdata,l):
+def datatask_correct(tdate,tdata,l):
     l = data_date_is_greater(tdate,l)
     if l >= 0:
         data_ok = data_is_greater(tdata,l)
         if data_ok >= 0:
             return l
+    return -1
+"""
+def datetime_correct(d, l):
+    data_date_greater_list = []
+    for i in range(0,len(l)):
+        data = l[i]
+        td = datetime.strptime(data["date"], '%Y-%m-%dT%H:%M:%S.%f')
+        print td
+        print d
+        if td > d:
+            print "AAAA"
+            data_date_greater_list.append(data)
+            return data_date_greater_list
+    return -1
+"""
+def datetimetask_correct(tdate, task_datetime, device_data_list):
+    task_datetime_list_ok = []
+    datetimenow = datetime.now()
+    task_datetime_list = device_data_list #datetime_correct(datetimenow, device_data_list)
+    if task_datetime_list >= 0:
+        for i in range(0,len(task_datetime_list)):
+            data = task_datetime_list[i]
+            td = datetime.strptime(task_datetime, '%Y-%m-%dT%H:%M:%S')
+            now = datetime.strptime(str(datetimenow), '%Y-%m-%d %H:%M:%S.%f')
+            if td < now:
+                task_datetime_list_ok.append(data)
+                return task_datetime_list_ok
     return -1
 
 def execute_tasks(task_queue, server_ip, server_port):
@@ -63,15 +112,33 @@ def execute_tasks(task_queue, server_ip, server_port):
         if device_data_get.status_code == 200:
             device_data = json.loads(device_data_get.text)
             device_data_list = device_data["data"]
-            task_data = dt['data_value']
-            task_date = datetime.strptime(task_date, '%Y-%m-%dT%H:%M:%S.%f')
-            data_ok = data_correct(task_date, task_data, device_data_list)
 
-            if data_ok >= 0:
-                execute_task_function(dt, server_ip, server_port)
+            if "data_value" in dt:
+                execute_datatasks(dt,task_date,device_data_list,server_ip, server_port)
+
+            if "datetime" in dt:
+                execute_datetimetasks(dt,task_date,device_data_list,server_ip, server_port)
 
 
-def execute_task_function(task, server_ip, server_port):
+def execute_datatasks(dt,td,dl,server_ip, server_port):
+    task_data = dt['data_value']
+    task_date = datetime.strptime(td, '%Y-%m-%dT%H:%M:%S.%f')
+    data_ok = datatask_correct(task_date, task_data, dl)
+
+    if data_ok >= 0:
+        execute_datatask_function(dt, server_ip, server_port)
+
+
+def execute_datetimetasks(dtt,td,dl,server_ip, server_port):
+    task_datetime = dtt['datetime']
+    task_date = datetime.strptime(td, '%Y-%m-%dT%H:%M:%S.%f')
+    data_ok = datetimetask_correct(task_date, task_datetime, dl)
+
+    if data_ok >= 0:
+        execute_datetimetask_function(dtt, server_ip, server_port)
+
+
+def execute_datatask_function(task, server_ip, server_port):
     print str(task["id"]) + " " + task["label"]
     task_function = task["taskfunction"]
     task_device = task["device"]
@@ -106,10 +173,51 @@ def execute_task_function(task, server_ip, server_port):
     else:
         print "Some error updating state of task" + task_function["name"]
 
+def execute_datetimetask_function(task, server_ip, server_port):
+    print str(task["id"]) + " " + task["label"]
+    task_function = task["taskfunction"]
+    task_device = task["device"]
+    task_state = task["taskstate"]
+
+    if task_function["name"] == "shutdown":
+        print task_function["name"] + " " + task_device["label"]
+        print "Enviar msg al servidor para apagar el dispostivo"
+        task_data = {'taskstate':'2', 'taskfunction': + task_function["id"], 'label': task["label"], 'description': task["description"], 'datetime': task["datetime"], 'device_mac': task_device["device_mac"], 'owner': 'root' }
+        device_data = {'device_ip': task_device["device_ip"], 'device_mac': task_device["device_mac"], 'devicestate': task_device["devicestate"]['id'], 'label': task_device["label"], 'owner': 'root'}
+        function_exec_res = requests.post("http://" + server_ip + ":" + server_port + "/devices/" + str(task_device["id"]) + "/shutdown", data=device_data)
+
+        if function_exec_res.status_code == 200:
+            update_task_state = requests.post("http://" + server_ip + ":" + server_port + "/tasks/datetimetasks/" + str(task["id"]) + "/update", data=task_data)
+        else:
+            print("Cannot apply function %s to device %s " % (str(task_function["name"]), str(task_device["label"])))
+
+    if task_function["name"] == "turnon":
+        print task_function["name"] + " " + task_device["label"]
+        print "Enviar msg al servidor para prender el dispostivo"
+        task_data = {'taskstate':'2', 'taskfunction': + task_function["id"], 'label': task["label"], 'description': task["description"], 'datetime': task["datetime"], 'device_mac': task_device["device_mac"], 'owner': 'root' }
+        device_data = {'device_ip': task_device["device_ip"], 'device_mac': task_device["device_mac"], 'devicestate': task_device["devicestate"]['id'], 'label': task_device["label"], 'owner': 'root'}
+        function_exec_res = requests.post("http://" + server_ip + ":" + server_port + "/devices/" + str(task["id"]) + "/turnon", data=device_data)
+
+        if function_exec_res.status_code == 200:
+            update_task_state = requests.post("http://" + server_ip + ":" + server_port + "/tasks/datetimetasks/" + str(task["id"]) + "/update", data=task_data)
+        else:
+            print("Cannot apply function %s to device %s " % (str(task_function["name"]), str(task_device["label"])))
+
+    if update_task_state.status_code == 200:
+        print task["label"] + " State Update!"
+    else:
+        print "Some error updating state of task" + task_function["name"]
+
+
 #remote_ip = "158.69.223.78"
 remote_ip = "localhost"
 port = "8000"
 
-data_tasks_q = get_datatasks_from_server(remote_ip,port)
-#datetime_task_q = get_datatasks_from_server(remote_ip,port)
-execute_tasks(data_tasks_q, remote_ip,port)
+#2data_tasks_q = get_datatasks_from_server(remote_ip,port)
+tasksq = get_tasks_from_server(remote_ip,port,"datetimetasks")
+#tasksq = get_tasks_from_server(remote_ip,port,"datetime")
+#datetime_task_q = get_datetimetasks_from_server(remote_ip,port)
+if tasksq:
+    execute_tasks(tasksq, remote_ip,port)
+else:
+    print "Error in get task from server"
