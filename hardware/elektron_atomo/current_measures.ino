a529e3e184ac6c6b66606796a71c7fd0061ab6d5
AgustinParmisano <agustinparmisano@gmail.com>
2018-11-27 20:42:06 -0300

    Update current_measures.ino

--- hardware/elektron_atomo/current_measures.ino
+++ hardware/elektron_atomo/current_measures.ino
@@ -1,85 +1,69 @@
 float func_read_current_sensor() {
-  const int sensorIn = A0;
-  int mVperAmp = 66; //66 para el sensor de 30A
-  
+  const int sensorIn = A0; //pin ADC del Nodemcu.
+  int mVperAmp = 66; //Sensibilidad del sensor de 30A según fabricante.
   double voltage = 0;
   double VRMS = 0;
   double AmpsRMS = 0;
-  float inputV = 220.0; // es la tensión que entrega la empresa distribuidora y se puede medir con otro medidor y configurarlo como parametro en el sistema por hora o por día
-  unsigned int pF = 100; //como se obtiene? es el cosphi y tiene que ser por parámetro porque no podemos sacarlo para cada aparat
+  float inputV = 220.0; //tensión de la red, podría ser enviado por parámetro para achicar el error. 
+  unsigned int pF = 100; //factor de potencia con coseno de Phi en 1.
   float WH = 0;
-
-  voltage = getVPP();
-  VRMS = voltage * 0.707;
-  AmpsRMS = (VRMS * 1000) / mVperAmp;
-  
-
-  WH = (inputV * AmpsRMS) * (pF / 100.0); 
-  
-  Serial.print(String(WH, 3)); 
-  Serial.print(" WH "); 
-  Serial.print(AmpsRMS);
-  Serial.print(" Amps RMS");
-  
-  return (WH);
+  voltage = getVPP(); //se obtiene la corriente media.
+  VRMS = voltage * 0.707; //se multiplica la corriente eficaz obtenida por valor cuadrático medio.
+  AmpsRMS = (VRMS * 1000) / mVperAmp; //se multiplica el voltaje RMS por mil para pasar de milivolts a volts y se divide por la sensibilidad del sensor.
+ 
+  WH = (inputV * AmpsRMS) * (pF / 100.0); //se multiplica la corriente eficaz por la tensión y el resultado por el factor de potencia (1 en nuestro caso) para obtener la potencia aparente.
+ 
+  return (WH); //se retorna la potencia aparente para ser enviada al sistema.
 }
-
-
 float getVPP()
 {
   float result;
-  int readValue;     //valor obtenido del sensor
-
+  int readValue;     
   int maximos[50];
   int minimos[50];
   float resultMax = 0;
   float resultMin = 0;
-  Metro sensor_metro = Metro(0.02);
+  Metro sensor_metro = Metro(0.02); //timer de 2 microsegundos.
 
-
-  for  (int i = 0; i < 50; ++i) {
+  for  (int i = 0; i < 50; ++i) { //se toman 50 ondas.
     
-    int maxValue = 0;     //pico positivo
-    int minValue = 1024;     //pico negativo  
-    uint32_t start_time = millis();
+    int maxValue = 0;     
+    int minValue = 1024;      
+    uint32_t start_time = millis(); //milisegundos actuales.
     
     if(sensor_metro.check()) {
-      while((millis()-start_time) < 20) //muestra de 0.02 segundos que es 1 ciclo de 50Hz (ciclo de onda sinusoidal)
-      {
+           while((millis()-start_time) < 20) //muestreo durante 0.02 segundos.
+           {
          
-         readValue = analogRead(C_SENSOR1);
-         // se actualizan los valores pico con cada muestra
-         if (readValue > maxValue) 
-         {
-             /*guardo el máximo valor*/
-             maxValue = readValue;
-         }
-         if (readValue < minValue) 
-         {
-             /*guardo el mínimo valor*/
-             minValue = readValue;
-         }
-      }
+               readValue = analogRead(C_SENSOR1); //se lee del sensor.
+                if (readValue > maxValue)
+               {
+                        maxValue = readValue; //pico máximo de la onda actual.
+                }
+                if (readValue < minValue)
+                    {
+                        minValue = readValue; //pico mínimo (negativo) de la onda actual.
+                    }
+           }
     }
-    maximos[i] = maxValue; //guardo los maximos de cada ciclo
-    minimos[i] = minValue; //guardo los minimos de cada ciclo
+    maximos[i] = maxValue; //se guardan los picos en sus arreglos.
+    minimos[i] = minValue;
     
   }
 
+   //se promedia cada pico.
    int promMax = 0;
    int promMin = 0;
-   for (int i = 0; i < 50; ++i) {
-       promMax = promMax + maximos[i];
+   for (int i = 0; i < 50; ++i) { 
+       promMax = promMax + maximos[i]; 
        promMin = promMin + minimos[i];
    }
    
-   resultMax = promMax / 50; 
+   resultMax = promMax / 50;
    resultMin = promMin / 50;
-  
-   // Obtengo el valor medio de los picos y los mapeo a los valores del ADC del Nodemcu (que entrega y recibe 3.3v) a valores 1024 digitales.
+ 
+   //se restan los promedios de los picos, los convierto a valores digitales según el voltaje del Nodemcu y los divido por 2 para tener su media (los picos mínimos son negativos por lo que se suman a los positivos en la resta).
    result = ((((resultMax - resultMin) * 3.3)/1024.0) / 2.0);
    
-   Serial.println();
-
    return result;
 }
